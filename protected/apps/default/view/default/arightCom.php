47e9010f0d89d5cf2dea610dea985deb67472b5b
LeslieWang <leslie.wang87@gmail.com>
2013-06-26 09:02:16 +0800

    Remove useless functions and add required functions.

--- protected/apps/default/view/default/arightCom.php
+++ protected/apps/default/view/default/arightCom.php
@@ -17,44 +17,17 @@
           <div class="bock-con">{piece:announce}</div>
        </div>
        
+       
+       <div class="block box">
+          <div class="bock-tit"><h2>{$sorts[100033]['name']}</h2><a class="more" href="{$sorts[100033]['url']}">more</a></div>
+             <ul class="bock-list">
+                 {newso:{table=(news) field=(id,title,color,addtime,method) column=(100033) where=(ispass='1') limit=(6)}}
+                   <li><a class="w220" style="color:[newso:color]" title="[newso:title]" target="_blank" href="[newso:url]">[newso:title $len=20]</a></li>
+                 {/newso}
+             </ul>
+       </div>
        <div class="block box">
           <div class="bock-tit"><h2>联系我们</h2></div>
           <div class="bock-con">{piece:contactus}</div>
-       </div>
-       
-        <!--演示调用固定栏目下的子栏目-->
-       <div class="block box">
-          <div class="bock-tit"><h2>{$sorts[100001]['name']}</h2></div>
-          <ul class="bock-list">
-            {loop $sorts $key $vo}  
-              {if (strpos($vo['path'],'100001,')!==false)}
-                {if ($vo['deep']- $sorts[100001]['deep'])==1}
-                  <li><a class="w180" title="{$vo['name']}"  href="{$vo['url']}">{$vo['name']}</a></li>
-                {elseif ($vo['deep']- $sorts[100001]['deep'])==2}
-                  <li><a class="w180" title="{$vo['name']}"  href="{$vo['url']}">|--{$vo['name']}</a></li>
-                {elseif ($vo['deep']- $sorts[100001]['deep'])==3}
-                  <li><a class="w180" title="{$vo['name']}"  href="{$vo['url']}">|--|--{$vo['name']}</a></li>
-                 {/if}
-              {/if}
-            {/loop}
-          </ul>
-       </div>
-       
-       <div class="block box">
-          <div class="bock-tit"><h2>热门文章</h2></div>
-          <ul class="bock-list">
-            {hot:{table=(news) field=(id,title,color,addtime,method) order=(hits desc,id desc) where=(ispass='1') limit=(7)}}
-                     <li><a class="w180" style="color:[hot:color]" title="[hot:title]" target="_blank" href="[hot:url]">[hot:title $len=25]</a><span>{date($hot['addtime'],Y-m-d)}</span></li>
-           {/hot}
-          </ul>
-       </div>
-       
-       <div class="block box">
-          <div class="bock-tit"><h2>推荐文章</h2></div>
-          <ul class="bock-list">
-           {recmd:{table=(news) field=(id,title,color,addtime,method) order=(recmd desc,id desc) where=(ispass='1') limit=(7)}}
-                     <li><a class="w180" style="color:[recmd:color]" title="[recmd:title]" target="_blank" href="[recmd:url]">[recmd:title $len=25]</a><span>{date($recmd['addtime'],Y-m-d)}</span></li>
-           {/recmd}
-          </ul>
        </div>
 </div> 
--- protected/apps/default/view/default/index_index.php
+++ protected/apps/default/view/default/index_index.php
@@ -10,49 +10,24 @@
 
 <div class="yx-g index-mid">
     <div class="yx-u-17-24">
-       <div class="yx-g index-mid-top">
-          <div class="yx-u-3-5">
-             <div class="index-reg box">
-              <ul class="bock-list">
-                    {news:{table=(news) field=(id,title,color,addtime,method,description) where=(ispass='1') limit=(6)}}
-                       {if $news_i==1} <!--通过计数器判断第一条显示为头条样式-->
-                          <h1><a href="[news:url]">[news:title $len=20]</a></h1>
-                          <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[news:description]</p>
-                       {else}
-                          <li><a class="w280" style="color:[news:color]" title="[news:title]" target="_blank" href="[news:url]">[news:title $len=25]</a><span>{date($news['addtime'],Y-m-d)}</span></li>
-                       {/if}
-                    {/news} 
-                </ul>
-             </div>
-          </div>
-       </div>
+    
        
        <div class="yx-u box index-big">
-            <div class="bock-tit"><h2>{$sorts[100002]['name']}</h2><a class="more" href="{$sorts[100002]['url']}">more</a></div>
-            <ul class="photo-list">
-             {photo:{table=(photo) field=(id,title,picture,method) column=(100002) where=(ispass='1') limit=(8)}}
-               <li><a style="color:[photo:color]" title="[photo:title]" target="_blank" href="[photo:url]"><img class="box" src="{$PhotoImgPath}thumb_[photo:picture]" alt="[photo:title]" width="145" height="110"></a><span>[photo:title]</span></li>
-             {/photo}
-            </ul>
-       </div>
-       
-       <div class="yx-u box index-big">
-            <div class="bock-tit"><h2>{$sorts[100004]['name']}</h2><a class="more" href="{$sorts[100004]['url']}">more</a></div>
-            <ul class="photo-list">
-             <!--演示调用拓展字段值-->
-             {photo:{table=(photo) field=(id,title,picture) exfield=(area,price) column=(100004) where=(ispass='1') limit=(8)}}
-               <li><a style="color:[photo:color]" title="[photo:title]" target="_blank" href="[photo:url]"><img class="box" src="{$PhotoImgPath}thumb_[photo:picture]" alt="[photo:title]" width="145" height="110"></a><span>[photo:title]</span><span> 价格：[photo:price]-[photo:area]</span></li>
-             {/photo}
-            </ul>
+            <div class="bock-tit"><h2>{$sorts[100032]['name']}</h2><a class="more" href="{$sorts[100032]['url']}">more</a></div>
+                     <ul class="bock-list">
+                     {newso:{table=(news) field=(id,title,color,addtime,method) column=(100032) where=(ispass='1') limit=(6)}}
+                       <li><a class="w220" style="color:[newso:color]" title="[newso:title]" target="_blank" href="[newso:url]">[newso:title $len=40]</a><span>{date($newso['addtime'],Y-m-d)}</span></li>
+                     {/newso}
+                     </ul>
        </div>
 
        <div class="yx-u index-big">
            <div class="yx-g">
               <div class="yx-u-1-2">
                  <div class="box left-list">
-                     <div class="bock-tit"><h2>{$sorts[100005]['name']}</h2><a class="more" href="{$sorts[100005]['url']}">more</a></div>
+                     <div class="bock-tit"><h2>{$sorts[100034]['name']}</h2><a class="more" href="{$sorts[100034]['url']}">more</a></div>
                      <ul class="bock-list">
-                     {newso:{table=(news) field=(id,title,color,addtime,method) column=(100005) where=(ispass='1') limit=(6)}}
+                     {newso:{table=(news) field=(id,title,color,addtime,method) column=(100034) where=(ispass='1') limit=(6)}}
                        <li><a class="w220" style="color:[newso:color]" title="[newso:title]" target="_blank" href="[newso:url]">[newso:title $len=16]</a><span>{date($newso['addtime'],Y-m-d)}</span></li>
                      {/newso}
                      </ul>
@@ -61,9 +36,9 @@
               
               <div class="yx-u-1-2">
                  <div class="box">
-                     <div class="bock-tit"><h2>{$sorts[100006]['name']}</h2><a class="more" href="{$sorts[100006]['url']}">more</a></div>
+                     <div class="bock-tit"><h2>{$sorts[100035]['name']}</h2><a class="more" href="{$sorts[100035]['url']}">more</a></div>
                      <ul class="bock-list">
-                       {newst:{table=(news) field=(id,title,color,addtime,method) column=(100006) where=(ispass='1') limit=(7)}}
+                       {newst:{table=(news) field=(id,title,color,addtime,method) column=(100035) where=(ispass='1') limit=(7)}}
                      <li><a class="w220" style="color:[newst:color]" title="[newst:title]" target="_blank" href="[newst:url]">[newst:title $len=16]</a><span>{date($newst['addtime'],Y-m-d)}</span></li>
                       {/newst}
                      </ul>
